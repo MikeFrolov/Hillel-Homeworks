17486f9c293d548d580775715068336845f4ea38
Michael Frolov <moyshedev@gmail.com>
2021-07-08 11:54:16 +0300

    Added .gitignore file, Implemented the creation and filling of the database in hw3_app.py file

--- hw3/hw3_app.py
+++ hw3/hw3_app.py
@@ -3,22 +3,39 @@ from flask_sqlalchemy import SQLAlchemy
 import csv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hw2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hw3.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-class Sales(db.Model):
+class Sales(db.Model):  # DB Model
 
     id = db.Column(db.Integer, primary_key=True)
-    Transaction_date = db.Column(db.String(60), unique=True, nullable=False)
-    Product = db.Column(db.String(60), unique=False, nullable=False)
-    Price = db.Column(db.String(80), unique=False, nullable=False)
-    Payment_Type = db.Column(db.String(40), unique=False, nullable=False)
+    Transaction_date = db.Column(db.String(60), unique=False)
+    Product = db.Column(db.String(60), unique=False)
+    Price = db.Column(db.String(80), unique=False)
+    Payment_Type = db.Column(db.String(40), unique=False)
 
     def __repr__(self):
-        return '<Customers %r>' % self.first_name
+        return '<Sales %r>' % self.Transaction_date
+
+
+db.create_all()  # Create DB
+
+with open("homework3sales.csv") as file:  # Writing data to the database
+    rows = csv.reader(file, delimiter=';')
+    selling = []
+    for row in rows:
+        selling.append(row)
+    for i, note in enumerate(selling[1:]):
+        rec_to_db = Sales(Transaction_date=note[0],
+                          Product=note[1],
+                          Price=note[2],
+                          Payment_Type=note[3]
+                          )
+        db.session.add(rec_to_db)
+    db.session.commit()
 
 
 if __name__ == '__main__':
